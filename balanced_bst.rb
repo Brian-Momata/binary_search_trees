2a8fb499ca3df63e7ce10f95e2195e4ab8b9def1
Brian-Momata <brianmomata@gmail.com>
2022-10-17 09:06:09 +0300

    Add #pretty_print method to visualize tree

--- balanced_bst.rb
+++ balanced_bst.rb
@@ -36,4 +36,12 @@ class Tree
       node.right == nil ? node.right = new_node : insert(value, node.right)
     end
   end
+
+  private
+   
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
